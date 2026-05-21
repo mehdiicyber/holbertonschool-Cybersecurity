8a584cbadd8186292743ac992035d4d851100016
mehdiicyber <13735@holbertonstudents.com>
2026-05-21 21:46:39 +0400

    Update 4-cidr2mask.sh

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "%08d.%08d.%08d.%08d\n" $(echo "obase=2; 2^32 - 2^(32-$1)" | bc | tr '01' '10') | tr '.' ' ' | while read -r a b c d; do echo "$((2#$a)).$((2#$b)).$((2#$c)).$((2#$d))"; done
+printf "%032d\n" $(echo "obase=2; 2^32 - 2^(32-$1)" | bc | tr '01' '10') | sed 's/.\{8\}/& /g' | while read -r a b c d; do echo "$((2#$a)).$((2#$b)).$((2#$c)).$((2#$d))"; done | sed 's/255/255/g'
