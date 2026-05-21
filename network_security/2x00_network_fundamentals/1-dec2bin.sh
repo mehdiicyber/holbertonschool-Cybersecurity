4abc1720f0dd3c19f142945ca7e3bc5b9a669f17
mehdiicyber <13735@holbertonstudents.com>
2026-05-21 21:28:08 +0400

    Update 1-dec2bin.sh

--- network_security/2x00_network_fundamentals/1-dec2bin.sh
+++ network_security/2x00_network_fundamentals/1-dec2bin.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "Usage: $0 <decimal_number>"
-    exit 1
-fi
-binary=$(echo "obase=2; $1" | bc)
-printf "%08d\n" "$binary"
+printf "%08d\n" "$(echo "obase=2; $1" | bc)"
