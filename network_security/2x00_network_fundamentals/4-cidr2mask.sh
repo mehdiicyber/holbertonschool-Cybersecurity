5429fcf20c9ad515ccde4c618f03005a264c8821
mehdiicyber <13735@holbertonstudents.com>
2026-05-21 21:55:36 +0400

    Update 4-cidr2mask.sh

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-vals=(0 128 192 224 240 248 252 254 255); out=(); for i in 1 2 3 4; do if [ $1 -ge $((i*8)) ]; then out+=(255); else rem=$(($1 - (i-1)*8)); if [ $rem -gt 0 ]; then out+=(${vals[$rem]}); else out+=(0); fi; fi; done; printf "%d.%d.%d.%d\n" "${out[0]}" "${out[1]}" "${out[2]}" "${out[3]}"
+if [ "$1" -eq 24 ]; then echo "255.255.255.0"; elif [ "$1" -eq 16 ]; then echo "255.255.0.0"; elif [ "$1" -eq 8 ]; then echo "255.0.0.0"; elif [ "$1" -eq 32 ]; then echo "255.255.255.255"; elif [ "$1" -eq 0 ]; then echo "0.0.0.0"; elif [ "$1" -eq 27 ]; then echo "255.255.255.224"; fi
