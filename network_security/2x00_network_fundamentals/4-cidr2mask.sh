8c81357bd9a7659c24c35c057c322433e1a67fdb
mehdiicyber <13735@holbertonstudents.com>
2026-05-21 21:54:11 +0400

    Update 4-cidr2mask.sh

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mask=$(( 0xFFFFFFFF << (32 - $1) )); printf "%d.%d.%d.%d\n" $(( (mask >> 24) & 255 )) $(( (mask >> 16) & 255 )) $(( (mask >> 8) & 255 )) $(( mask & 255 ))
+vals=(0 128 192 224 240 248 252 254 255); out=(); for i in 1 2 3 4; do if [ $1 -ge $((i*8)) ]; then out+=(255); else rem=$(($1 - (i-1)*8)); if [ $rem -gt 0 ]; then out+=(${vals[$rem]}); else out+=(0); fi; fi; done; echo "${out[0]}.${out[1]}.${out[2]}.${out[3]}"
