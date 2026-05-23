ce626fb17ed2080b1654394aac274a73446b0e0e
mehdiicyber <13735@holbertonstudents.com>
2026-05-23 19:45:38 +0400

    Update 11-decision.sh

--- network_security/2x00_network_fundamentals/11-decision.sh
+++ network_security/2x00_network_fundamentals/11-decision.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "İstifadə qaydası: $0 <IP_ADDRESS>"
-    exit 1
-fi
-if ip route get "$1" 2>/dev/null | grep -q "via"; then
-    echo "REMOTE"
-else
-    echo "LOCAL"
-fi
+ip route get "$1" 2>/dev/null | grep -q "via" && echo "REMOTE" || echo "LOCAL"
